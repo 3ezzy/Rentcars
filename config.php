13734d956bcf724da0751b9a862703051c3928dd
Mehdi <khammali26@gmail.com>
2024-12-10 11:19:03 +0100

    link PHP with MySQl

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '123';
+$dbname = 'projet1';
+
+$connection = new mysqli($host,$username,$password,$dbname);
+
+/*if (!$connection){
+    echo "connection failed";
+}else{
+    echo "connection good";
+}*/
+
+
+?>
