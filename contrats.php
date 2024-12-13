acf56967b027f0e6dbb16497962d7a390a988c02
Mehdi <khammali26@gmail.com>
2024-12-13 22:33:41 +0100

    add buttons to add user & car & contrat

--- contrats.php
+++ contrats.php
@@ -22,10 +22,15 @@
         include "sidebar.php"
         ?>
         <main class="sm:ml-64 p-4">
-            <h1 class="flex items-center gap-2 text-3xl font-extrabold text-blue-600 mb-2">
-                <i class="fa-solid fa-file-contract text-3xl"></i>
-                <span>List of Contracts</span>
-            </h1>
+        <div class="flex items-center justify-between mb-4">
+                <h1 class="flex items-center gap-2 text-3xl font-extrabold text-blue-600">
+                    <i class="fa-solid fa-file-contract text-3xl"></i>
+                    <span>List of Contracts</span>
+                </h1>
+                <a href="createContract.php" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Add Contract
+                </a>
+            </div>
 
 
             <?php
